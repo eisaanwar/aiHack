030030e6e0a480b8e14186480b2d564324863615
Eisa Abdullah Masud Anwar <e.anwar@se16.qmul.ac.uk>
2018-11-17 23:28:14 +0000

    Create test.m
    this is fun

---
+++ test.m
@@ -0,0 +1,20 @@
+T = readtable('copy.csv');
+id = {'NA' '' -99 NaN Inf};
+sumMiss = sum(ismissing(T,id));
+per = sumMiss / 353546 * 100;
+array = {};
+for n=1:length(per)
+    element = per(n);
+    if element > 80
+       array{end+1} = T.Properties.VariableNames{n};
+    end
+end
+
+for n1=1:length(array)
+    element2 = array{n1};
+    T(:,element2) = [];
+    head(T,353546);
+end
+
+writetable(T,'myData.csv');
+type 'myData.csv';
